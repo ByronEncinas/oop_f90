46a26773da20d6ac7a5a393719fcde24a2a4a80c
ByronEncinas <byron_encinas@outlook.com>
2026-04-19 18:21:57 -0700

    some physical constants from NIST databank

---
+++ src/const.f90
@@ -0,0 +1,16 @@
+Module consts
+    
+	use iso_fortran_env, only: real64, real128
+
+	!! https://physics.nist.gov/cuu/Constants/links.html
+
+	implicit none
+
+	!! real64 can go over to ~1e-300
+	
+	real(real64), parameter :: c_si  = 299792458_readl64
+        real(real64), parameter :: G_si  = 6.6743015e-11_readl64   
+        real(real64), parameter :: qe_si = 1.6021766634e-19_readl64
+        real(real64), parameter :: me_si = 9.109383713928e-31_real64
+
+End Module consts
